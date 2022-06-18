e7fecab0ab3e09a386c7494bdc6af9207ee461ce
Kuroi Kyu <kuroikyu@users.noreply.github.com>
2022-06-18 16:22:03 +0200

    feat: add basic functionality

--- src/generateProductSubstitutesMap.ts
+++ src/generateProductSubstitutesMap.ts
@@ -26,8 +26,103 @@ export function generateProductSubstitutesMap(
   const productsIndexed = generateProductsByIdIndex(products); //This is for getting a product by id with O(1) cost
 
   // YOUR CODE GOES HERE
+  products.forEach((product) => {
+    const result = getSubstituteProductRecursive({
+      products,
+      initialPosition: products.indexOf(product),
+      currentPosition: undefined,
+      now,
+    });
+
+    if (result === -1) {
+      productsWithErrors.push(product.id);
+    } else if (result) {
+      map.set(product.id, result);
+    }
+  });
 
   return { map, productsWithErrors };
+}
+
+interface GetSubstituteProductRecursive {
+  products: ReadonlyArray<Product>;
+  initialPosition: number;
+  currentPosition: number | undefined;
+  now: Date;
+}
+
+function getSubstituteProductRecursive({
+  products,
+  initialPosition,
+  currentPosition,
+  now = new Date(),
+}: GetSubstituteProductRecursive): string | undefined | -1 {
+  if (typeof currentPosition === 'undefined') {
+    const currentProduct = products[initialPosition];
+
+    // RESULT: No substitute found
+    if (!currentProduct.replacementProductId) {
+      return undefined;
+    }
+
+    // ERROR: Replacement but no replacement date
+    if (!currentProduct.replacementDate) {
+      return -1;
+    }
+
+    if (dateFns.isAfter(currentProduct.replacementDate, now)) {
+      return undefined;
+    }
+
+    const replacementProduct = products.find(
+      (repl) => repl.id === currentProduct.replacementProductId
+    );
+
+    // ERROR: replacementProductId not valid
+    if (!replacementProduct) {
+      return -1;
+    }
+
+    return getSubstituteProductRecursive({
+      products,
+      initialPosition,
+      currentPosition: products.indexOf(replacementProduct),
+      now,
+    });
+  } else {
+    const currentProduct = products[currentPosition];
+
+    // RESULT: Substitute found!
+    if (!currentProduct.replacementProductId) {
+      return currentProduct.id;
+    }
+
+    // ERROR: Replacement but no replacement date
+    if (!currentProduct.replacementDate) {
+      return -1;
+    }
+
+    // RESULT: Substitute found despite replacement date
+    if (dateFns.isAfter(currentProduct.replacementDate, now)) {
+      return currentProduct.id;
+    }
+
+    const replacementProduct = products.find(
+      (repl) => repl.id === currentProduct.replacementProductId
+    );
+
+    // ERROR: replacementProductId not valid
+    if (!replacementProduct) {
+      return -1;
+    }
+
+    return getSubstituteProductRecursive({
+      products,
+      initialPosition,
+      currentPosition: products.indexOf(replacementProduct),
+      now,
+    });
+  }
 }
 
 /**
